06346ad0ee87275767295b9cce48c297650590ec
Mitch Skinner <git@arctur.us>
2013-05-08 17:33:09 -0700

    add MPL2 license

--- js/spinteny.js
+++ js/spinteny.js
@@ -1,3 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 goog.require("goog.asserts");
 goog.require("goog.dom");
 goog.require("goog.debug.Logger");
